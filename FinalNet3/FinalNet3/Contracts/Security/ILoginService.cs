7bccce9f139df5ccd0b66318ac447e74bc8cdcb8
Alvaro0295 <lotero021095@gmail.com>
2017-09-21 18:36:45 -0500

    Ya

---
+++ FinalNet3/FinalNet3/Contracts/Security/ILoginService.cs
@@ -0,0 +1,15 @@
+﻿using FinalNet3.DTO.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalNet3.Contracts.Security
+{
+    /*Contracto - INTERFAZ que obliga a definir el logIn*/
+    interface ILoginService
+    {
+        IList<String> LoginUser(LoginDTO login);
+    }
+}
